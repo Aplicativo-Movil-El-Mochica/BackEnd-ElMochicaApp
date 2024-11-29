cba56882267dc06193243b1eceade7a4762555c8
Juan Gutierrez <81880988+Ghost-ofc@users.noreply.github.com>
2024-11-29 11:37:53 -0500

    Update JwtTokenUtil.java

--- Microservicio-Usuario/src/main/java/org/mochica/AppDelivery/Config/JwtTokenUtil.java
+++ Microservicio-Usuario/src/main/java/org/mochica/AppDelivery/Config/JwtTokenUtil.java
@@ -23,7 +23,7 @@ public class JwtTokenUtil implements Serializable {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtTokenUtil.class);
     private static final long serialVersionUID = 7383112237L;
-    public static final int JWT_TOKEN_VALIDITY = 60*60;         // 10 minutes
+    public static final int JWT_TOKEN_VALIDITY = 604800;         // 10 minutes
 
     @Value("${jwt.secret}")
     public String secret;
